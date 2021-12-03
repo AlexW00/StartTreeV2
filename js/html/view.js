de0147457c72851be2634dab9cb51ba492273f03
AlexW00 <55558407+AlexW00@users.noreply.github.com>
2021-12-03 01:00:12 +0100

    zzz

--- js/html/view.js
+++ js/html/view.js
@@ -1,10 +1,11 @@
-import Tree from "./js/views/tree/components/tree.js";
+import Tree from "/js/views/tree/components/tree.js";
 import { parse } from "../helper/jsurl.js";
 
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const treeConfig = parse(urlParams.get("t")) || {};
 
+console.log(treeConfig);
 const t = new Tree(treeConfig, false);
 document.body.appendChild(t.html());
 
