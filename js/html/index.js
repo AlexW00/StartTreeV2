6bb7fe00931cd98fbfd47b27d2185a27a90bea63
AlexW00 <55558407+AlexW00@users.noreply.github.com>
2021-12-06 10:14:39 +0100

    🐛 Updated start

--- js/html/index.js
+++ js/html/index.js
@@ -6,4 +6,4 @@ const exampleData = await fetch("./js/views/tree/exampleConfig.json").then(
 
 const jsonStringified = stringify(exampleData);
 
-location.href = `./html/e.html?t=${jsonStringified}`;
+document.location.replace(`./html/e.html?t=${jsonStringified}`);
