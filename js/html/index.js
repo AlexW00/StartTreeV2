ffacd1c2f3147310b6892d7b4559eeab7351c777
AlexW00 <55558407+AlexW00@users.noreply.github.com>
2021-12-06 10:06:48 +0100

    🔧 Updated default page

--- js/html/index.js
+++ js/html/index.js
@@ -6,12 +6,4 @@ const exampleData = await fetch("./js/views/tree/exampleConfig.json").then(
 
 const jsonStringified = stringify(exampleData);
 
-/* const button = document.createElement("h1");
-const a = document.createElement("a");
-
-a.href = `v.html?t=${jsonStringified}`;
-a.innerText = "Open tree";
-button.appendChild(a);
-document.body.appendChild(button); */
-
-location.href = `./html/v.html?t=${jsonStringified}`;
+location.href = `./html/e.html?t=${jsonStringified}`;
